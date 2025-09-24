644a4946c20c997f38827887aa6f86f31b36ff4a
Shaharyar <shaharyar4t4@gmail.com>
2025-09-24 23:14:00 +0500

    initall comint

---
+++ app/screen/Premission.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Premission = () => {
+  return (
+   <View>
+    <Text>Premission Screen</Text>
+   </View>
+  )
+}
+
+export default Premission
