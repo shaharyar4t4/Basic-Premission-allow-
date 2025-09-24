6079a65915afbf445f681b1e0fadc608ba12d370
Shaharyar <shaharyar4t4@gmail.com>
2025-09-25 00:32:18 +0500

    updated

---
+++ app/navigation/AllScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+  Home: undefined;
+    Premission: undefined;  
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AllScreen = () => {
+  return (
+    
+  )
+}
+
+export default AllScreen
