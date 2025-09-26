bad0192a27f22dbae7d23d99d348d9c5d7228196
Shaharyar <shaharyar4t4@gmail.com>
2025-09-27 01:19:17 +0500

    add the video picker componets

--- app/screen/Premission.tsx
+++ app/screen/Premission.tsx
@@ -1,15 +1,20 @@
 import MainLayout from '@/components/Mainlayout/MainLayout';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Video } from 'expo-av';
 import * as ImagePicker from 'expo-image-picker';
 import React, { useState } from 'react';
 import { Button, Image, StyleSheet, Text, View, } from 'react-native';
 import { RootStackParamList } from '../navigation/AllScreen';
 
+// npm i expo-image-picker (help to access the device's image library and camera)
+// npm install expo-av (help to play audio and video files in your app)
 
 type PropsType = NativeStackScreenProps<RootStackParamList, "Premission">;
 
 const Premission = ({ navigation }: PropsType) => {
     const [image, setImage] = useState<string | null>(null);
+    const [video, setVideo]= useState<string | null>(null);
+
 
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
@@ -23,7 +28,14 @@ const Premission = ({ navigation }: PropsType) => {
         console.log(result);
 
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
+            // check the condition picker is image or video
+            const assets= result.assets[0];
+            if(assets.type === 'video'){
+                setVideo(assets.uri);
+            }
+            if(assets.type === 'image'){
+                setImage(assets.uri);
+            }
         }
     };
     return (
@@ -34,7 +46,10 @@ const Premission = ({ navigation }: PropsType) => {
 
                 </View>
                 <View>
+                    {/*for image selector*/}
                     {image && <Image source={{ uri: image }} style={styles.image} />}
+                    {/* for video selector */}
+                    {video && <Video useNativeControls source={{ uri: video }} style={styles.image}></Video>}
                     <Button title="Pick an image from camera roll" onPress={pickImage} />
                     
                 </View>
